9dc293e76087280dec9df69f6928e2863626ac78
TramsizedInc <kavcsicsabcsi@gmail.com>
2024-04-04 11:54:08 +0200

    initial commit

---
+++ mapdata.js
@@ -0,0 +1,214 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+    //General settings
+		//width: "300", //or 'responsive'
+		width: "600", //or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    pop_ups: "detect",
+    
+		//State defaults
+		state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+		//Location defaults
+		location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+		//Label defaults
+		label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+		//Zoom settings
+		zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+		//Popup settings
+		popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+		//Advanced settings
+		div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website"
+  },
+  state_specific: {
+    HUN3138: {
+      name: "Vas",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    HUN3155: {
+      name: "Baranya"
+    },
+    HUN3156: {
+      name: "Gyor-Moson-Sopron"
+    },
+    HUN3157: {
+      name: "Somogy"
+    },
+    HUN3158: {
+      name: "Veszprém"
+    },
+    HUN3159: {
+      name: "Zala"
+    },
+    HUN3160: {
+      name: "Budapest"
+    },
+    HUN3161: {
+      name: "Bács-Kiskun"
+    },
+    HUN3162: {
+      name: "Fejér"
+    },
+    HUN3163: {
+      name: "Komárom-Esztergom"
+    },
+    HUN3164: {
+      name: "Pest"
+    },
+    HUN3165: {
+      name: "Tolna"
+    },
+    HUN3166: {
+      name: "Nógrád"
+    },
+    HUN3168: {
+      name: "Borsod-Abaúj-Zemplén"
+    },
+    HUN3169: {
+      name: "Szabolcs-Szatmár-Bereg"
+    },
+    HUN3171: {
+      name: "Békés"
+    },
+    HUN3172: {
+      name: "Csongrád"
+    },
+    HUN3173: {
+      name: "Hajdú-Bihar"
+    },
+    HUN3174: {
+      name: "Heves"
+    },
+    HUN3175: {
+      name: "Jász-Nagykun-Szolnok"
+    },
+    HUN4903: {
+      name: "Szombathely"
+    },
+    HUN4904: {
+      name: "Sopron"
+    },
+    HUN4905: {
+      name: "Gyôr"
+    },
+    HUN4906: {
+      name: "Tatabánya"
+    },
+    HUN4907: {
+      name: "Érd"
+    },
+    HUN4908: {
+      name: "Székesfehérvár"
+    },
+    HUN4909: {
+      name: "Veszprém"
+    },
+    HUN4910: {
+      name: "Zalaegerszeg"
+    },
+    HUN4911: {
+      name: "Nagykanizsa"
+    },
+    HUN4912: {
+      name: "Kaposvár"
+    },
+    HUN4913: {
+      name: "Pécs"
+    },
+    HUN4914: {
+      name: "Szekszárd"
+    },
+    HUN4915: {
+      name: "Kecskemét"
+    },
+    HUN4916: {
+      name: "Szeged"
+    },
+    HUN4917: {
+      name: "Hódmezôvásárhely"
+    },
+    HUN4918: {
+      name: "Szolnok"
+    },
+    HUN4919: {
+      name: "Békéscsaba"
+    },
+    HUN4920: {
+      name: "Eger"
+    },
+    HUN4921: {
+      name: "Salgótarján"
+    },
+    HUN4922: {
+      name: "Miskolc"
+    },
+    HUN4923: {
+      name: "Nyíregyháza"
+    },
+    HUN4924: {
+      name: "Debrecen"
+    },
+    HUN4925: {
+      name: "Dunaújváros"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "47.5",
+      lng: "19.083333",
+      name: "Budapest"
+    }
+  }
+};
